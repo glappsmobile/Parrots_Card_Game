15d0b7132dd47f549072380a00bcd9004a82dc00
unknown <glappsmobile@gmail.com>
2021-07-27 20:37:05 -0300

    gamification implemented

--- scripts/consts.js
+++ scripts/consts.js
@@ -1,1 +1,3 @@
 const parrots = ["bobrossparrot", "explodyparrot", "fiestaparrot", "metalparrot", "revertitparrot", "tripletsparrot", "unicornparrot"];
+
+const delay = 1000;
--- scripts/script.js
+++ scripts/script.js
@@ -1,48 +1,113 @@
+let inSelection = false;
+
+let cards = [];
+let match = "";
+let lastCard;
+let block = false;
+let score = 0;
+let playCount = 0;
+let maxScore = 0;
+
 function gerarCartas(length){
+    maxScore = length;
+
     defineCards(length);
     let container = document.querySelector(".ctn-cards");
 
     for(i = 0; i < length; i++){
      container.innerHTML += 
      `<li class="card" onclick="reveal(this)">
+     <div class="front-face face">
         <img src="images/front.png"/>
+     </div>
+     <div class="back-face face">
+        <img src="images/${cards[i]}.gif"/>
+     </div>
+  
      </li>`;
     }
 }
 
 function askSize(){
     let length = Number(prompt("Com quantas cartas deseja jogar? (Par entre 4 e 14)"));
-    let isNumber = typeof(length) == "number";
+    let isPair = Boolean(length % 2 === 0);
+    let isCorrectLength = Boolean(length >= 4 && length <= 14);
 
-    if (isNumber){
-        let isPair = length % 2 == 0;
-        let isCorrectLength = length <= 14 && length >= 4;
-
-        if(isPair && isCorrectLength) {
-            gerarCartas(length);
-        } else {
-            askSize();
-        }
+    if(isPair && isCorrectLength) {
+        maxScore = length;
+        console.log(maxScore);
+        gerarCartas(length);
+    } else {
+        askSize();
     }
 }
 
 function reveal(card){
-    card.onClick = unreveal(card);
-    let img = card.querySelector("img");
-    let index = getChildIndex(card) - 1;
-    img.src = `images/${cards[index]}.gif`;
-    card.setAttribute( "onClick", "unreveal(this)");
+    console.log("-----");
+    console.log("block: "+block);
+    playCount++;
+    if (!block){
+        let index = getChildIndex(card);
+        let cardValue = cards[index];
+        card.classList.add("active");
+        console.log("last card: ");
+        console.log(lastCard);
+
+        if (lastCard !== undefined){
+            if (cardValue == match) {
+                console.log("MATCH");
+                match = "";
+                lastCard = undefined;
+                score += 2;
+                if (score == maxScore) gameOver();
+
+            } else {
+                console.log("ERRADO");
+                unreveal(card, lastCard);
+                blockAction();
+            }
+            lastCard = undefined;
+        }else{
+            lastCard = card;
+            match = cardValue;
+            card.setAttribute( "onClick", "");
+        }
+    }
+
 }
 
+function blockAction(){
+    block = true;
+    setTimeout(() => {  
+        block = false;
+    }, delay);
+
+}
+
+function gameOver(){
+    setTimeout(() => {  
+        alert(`Você ganhou em ${playCount} jogadas!`);
+    }, delay/2);
+}
+
+function unreveal(card1, card2) {
+    setTimeout(() => {  
+    card1.classList.remove("active");
+    card2.classList.remove("active");
+    card1.setAttribute( "onClick", "reveal(this)");
+    card2.setAttribute( "onClick", "reveal(this)");
+    block = false;
+}, delay);
+
+}
+/*
 function unreveal(card) {
-    let img = card.querySelector("img");
-    let index = getChildIndex(card) - 1;
-    img.src = `images/front.png`;
-    card.setAttribute( "onClick", "reveal(this)" );
+    card.classList.remove("active");
 }
+*/
 
-function getChildIndex(child){
-    let index = 0;
+function getChildIndex(child){ 
+    let index = -1;
     while( (child = child.previousSibling) != null ) index++;
 
     return index;
@@ -60,6 +125,10 @@ function doubleArray(array){
 
 function comparador(){
     return Math.random() - 0.5;
+}
+
+function getMaxScore(){
+
 }
 
 function defineCards(length){
@@ -81,8 +150,7 @@ function defineCards(length){
     cards.sort(comparador);
 }
 
-let cards = [];
 
 askSize();
-//gerarCartas(4);
+//gerarCartas(6);
 
